9d665afcbfab84e450396b66b2c194d5782819e3
GANZA <ganzanezaaloys35@gmail.com>
2023-12-15 15:33:51 +0200

    Committing changes in my_projects

---
+++ index.js
@@ -0,0 +1,5 @@
+let MenuBtn = document.getElementById('MenuBtn');
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark');
+});
