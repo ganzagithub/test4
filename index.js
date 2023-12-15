ccae58c4052688e351d416bde8fcedebc6795c90
GANZA <ganzanezaaloys35@gmail.com>
2023-12-15 18:09:49 +0200

    there is change commit

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Wait for the DOM to be fully loaded before accessing elements
     let MenuBtn = document.getElementById('MenuBtn');
 
     if (MenuBtn) {
         MenuBtn.addEventListener('click', function(e) {
-            // Ensure the body element is found before toggling the class
             let bodyElement = document.querySelector('body');
 
             if (bodyElement) {
                 bodyElement.classList.toggle('mobile-nav-active');
             }
 
-            // Toggle the 'fa-xmark' class on MenuBtn
+            // Toggle between 'fa-bars' and 'fa-xmark'
+            this.classList.toggle('fa-bars');
             this.classList.toggle('fa-xmark');
         });
     }
-    
+
     // Your Typed.js code goes here (assuming the library is properly loaded)
     let typed = new Typed('.auto-input', {
         strings: ['front-end developer!', 'freelancer!', 'UI Designer!', 'youtuber!'],
